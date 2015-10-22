a2ba0ec6990db5dc060d49f8fc0b30c6b45795a6
NanditaDamaraju <NanditaDamaraju@mssm.edu>
2015-10-22 18:54:37 -0400

    updated paths

--- save_ffn.py
+++ save_ffn.py
@@ -1,8 +1,13 @@
+import os
+import sys
+path = os.getcwd()
+sys.path.append(path)
+
 import pandas as pd
 import numpy as np
 from sklearn.metrics import roc_auc_score, accuracy_score
 log_transformed_ic50_cutoff = 1 - np.log(500)/np.log(5000)
-from pan_allele.helpers.data_helpers import *
+from pan_allele.helpers.pan_allele_data_helpers import *
 from pan_allele.helpers.feedforward_models import ffn_matrix, build_graph_native_sequence_model
 from pan_allele.helpers.sequence_encoding import padded_indices
 from pan_allele.helpers.amino_acid import amino_acid_letter_indices
@@ -10,14 +15,13 @@ from pan_allele.helpers.generate_pseudo_sequences import create_fasta_file
 import keras
 from sys import argv
 
-import os
-path = os.getcwd()
+
 
 class LossHistory(keras.callbacks.Callback):
 
     def on_epoch_end(self, epoch, logs={}):
         model_save = self.model
-        model_save.save_weights('pan_allele/weights/weights_ffn/weights' + str(epoch),overwrite=True)
+        model_save.save_weights('weights/weights_ffn/weights' + str(epoch),overwrite=True)
 
 def normalize_allele_name(allele_name):
     allele_name = allele_name.upper()
@@ -34,7 +38,7 @@ def normalize_allele_name(allele_name):
     return allele_name
 create_fasta_file(path, remove_residues = True, consensus_cutoff = 0)
 allele_groups, df = load_binding_data('pan_allele/files/bdata.2009.mhci.public.1.txt')
-allele_sequence_data, max_allele_length = load_allele_sequence_data(path +'pan_allele/files/pseudo/pseudo_sequences.fasta')
+allele_sequence_data, max_allele_length = load_allele_sequence_data('pan_allele/files/pseudo/pseudo_sequences.fasta')
 allele_list = sorted(create_allele_list(allele_groups, allele_sequence_data))
 peptide_train, mhc_train, Y_train = get_model_data(allele_list,
                                                     allele_sequence_data,
