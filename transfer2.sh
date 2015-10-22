a44a1f66eb4a34a424702a15054408157ce299c7
NanditaDamaraju <NanditaDamaraju@mssm.edu>
2015-10-22 15:00:27 -0400

    deleted unwanted scripts

--- transfer2.sh
+++
@@ -1,3 +0,0 @@
-scp -i ~/amazon_keys/DNN.pem ~/Intern/mhclearn/py/pan_allele/files/*.* ubuntu@ec2-52-26-177-233.us-west-2.compute.amazonaws.com:~/py/pan_allele/files/
-scp -i ~/amazon_keys/DNN.pem ~/Intern/mhclearn/py/pan_allele/*.py ubuntu@ec2-52-26-177-233.us-west-2.compute.amazonaws.com:~/py/pan_allele/
-scp -i ~/amazon_keys/DNN.pem ~/Intern/mhclearn/py/Spearmint/*.* ubuntu@ec2-52-26-177-233.us-west-2.compute.amazonaws.com:~/hyperparam/
