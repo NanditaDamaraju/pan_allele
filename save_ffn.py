f95273786241a997958c24f8f9d60b6a7cfce218
NanditaDamaraju <NanditaDamaraju@mssm.edu>
2015-10-23 13:05:52 -0400

    change to mult

--- save_ffn.py
+++ save_ffn.py
@@ -36,10 +36,17 @@ def normalize_allele_name(allele_name):
     for pattern in patterns:
         allele_name = allele_name.replace(pattern, "")
     return allele_name
-create_fasta_file(path, remove_residues = True, consensus_cutoff = 0)
+
+hyperparameters =  {'cutoff':[ 0], 'dropouts': [ 0.17621593,  0.        ,  0.   ], 'sizes': [ 16, 128,  99, 128, 102], 'mult_size': [32, 15]}
+#hyperparameters = {'cutoff':[ 0.33711265], 'dropouts': [ 0. ,  0.0254818 ,  0.10669398], 'sizes': [ 53,  82, 103,  74, 106, 59]}
+
+create_fasta_file(path, remove_residues = True, consensus_cutoff = hyperparameters['cutoff'])
+
+
 allele_groups, df = load_binding_data('pan_allele/files/bdata.2009.mhci.public.1.txt')
 allele_sequence_data, max_allele_length = load_allele_sequence_data('pan_allele/files/pseudo/pseudo_sequences.fasta')
 allele_list = sorted(create_allele_list(allele_groups, allele_sequence_data))
+
 peptide_train, mhc_train, Y_train = get_model_data(allele_list,
                                                     allele_sequence_data,
                                                     allele_groups,
@@ -48,10 +55,9 @@ peptide_train, mhc_train, Y_train = get_model_data(allele_list,
                                                     mhc_length=max_allele_length,
                                                     mhc_dense = None
                                                     )
-#hyperparameters =  {'cutoff':[ 0], 'dropouts': [ 0.17621593,  0.        ,  0.   ], 'sizes': [ 16, 128,  99, 128, 102], 'mult_size': [32, 15]}
-hyperparameters = {'cutoff':[ 0.33711265], 'dropouts': [ 0. ,  0.0254818 ,  0.10669398], 'sizes': [ 53,  82, 103,  74, 106, 59]}
-graph = build_graph_native_sequence_model(hyperparameters=hyperparameters, maxlen_mhc=max_allele_length)
-#graph = ffn_matrix(hyperparameters= hyperparameters, maxlen_mhc=max_allele_length)
+#graph = build_graph_native_sequence_model(hyperparameters=hyperparameters, maxlen_mhc=max_allele_length)
+
+graph = ffn_matrix(hyperparameters= hyperparameters, maxlen_mhc=max_allele_length)
 history = LossHistory()
 graph.fit(
                     {'peptide':peptide_train, 'mhc':mhc_train, 'output': Y_train},
@@ -61,13 +67,3 @@ graph.fit(
                     callbacks=[history]
 
     )
-
-
-# In[48]
-
-# In[ ]:
-
-
-
-
-# In[ ]:
