c0b2c729d009d0279771eadd71ac5b5ab6b20dab
NanditaDamaraju <NanditaDamaraju@mssm.edu>
2015-11-03 14:50:49 -0500

    argument parsing

--- hyperparameters_lr_batch_epoch.py
+++ hyperparameters_lr_batch_epoch.py
@@ -1,43 +1,51 @@
 import os
 import sys
+from sys import argv
+
 path = os.getcwd()
 sys.path.append(path)
 
 import pandas as pd
 import numpy as np
+
 from sklearn.metrics import roc_auc_score, accuracy_score
-log_transformed_ic50_cutoff = 1 - np.log(500)/np.log(5000)
 from pan_allele.helpers.pan_allele_data_helpers import *
-from pan_allele.helpers.feedforward_models import ffn_matrix, build_graph_native_sequence_model
-from pan_allele.helpers.convolution_model import convolution_graph_matrix
-from pan_allele.helpers.sequence_encoding import padded_indices
-from pan_allele.helpers.amino_acid import amino_acid_letter_indices
-from pan_allele.helpers.generate_pseudo_sequences import create_fasta_file
-from metrics import read_tcell_predictions, make_prediction
+from pan_allele.helpers.hyperparameters import get_graph_from_hyperparameters
+
 import keras
 import collections
-from sys import argv
+import argparse
 
+parser = argparse.ArgumentParser()
 
+parser.add_argument(
+    "--batch_size",
+    default=32,
+    help="minibatch size for training model (int)")
+
+parser.add_argument(
+    "--pred",
+    default='ffn_mult',
+    help="neural network type, `ffn_concat`, `ffn_mult` or `conv_mult`")
+
+parser.add_argument(
+    "--epochs",
+    default=200,
+    help="number of epochs to train upto")
 
 
 class LossHistory(keras.callbacks.Callback):
 
-    def metrics(self, batch_size, lr):
+    def metrics(self, batch_size):
         self.batch_size = batch_size
-        self.lr = lr
 
     def on_epoch_end(self, epoch, logs={}):
         model_save = self.model
-        model_save.save_weights('weights/ffn_concat/weights' + str(self.batch_size)+ '_' + str(self.lr) + '_'  + str(epoch),overwrite=True)
+        model_save.save_weights('weights/ffn_concat/weights' + str(self.batch_size)+ '_' + str(epoch),overwrite=True)
 
+def save_model(graph, batch_size,nb_epoch):
 
-
-def save_ffn(hyperparameters, batch_size=32, lr=0.001):
-
-    allele_groups, df = load_binding_data('pan_allele/files/bdataed')
-
-    create_fasta_file(path, remove_residues = True, consensus_cutoff = hyperparameters['cutoff'][0])
+    allele_groups, df = load_binding_data('pan_allele/files/bdata.2009.mhci.public.1.txt')
     allele_sequence_data, max_allele_length = load_allele_sequence_data('pan_allele/files/pseudo/pseudo_sequences.fasta')
     allele_list = sorted(create_allele_list(allele_groups, allele_sequence_data))
 
@@ -50,64 +58,28 @@ def save_ffn(hyperparameters, batch_size=32, lr=0.001):
                                                         mhc_dense = None
                                                         )
 
-    optimizer = keras.optimizers.RMSprop(lr=lr, rho=0.9, epsilon=1e-6)
-    graph = build_graph_native_sequence_model(hyperparameters=hyperparameters, maxlen_mhc = max_allele_length,optimizer = optimizer)
 
-    #graph = ffn_matrix(hyperparameters= hyperparameters, maxlen_mhc=max_allele_length, optimizer = optimizer)
     history = LossHistory()
-    history.metrics(batch_size, lr)
+    history.metrics(batch_size)
     graph.fit(
                         {'peptide':peptide_train, 'mhc':mhc_train, 'output': Y_train},
                         batch_size=batch_size,
-                        nb_epoch=200,
+                        nb_epoch=nb_epoch,
                         verbose = 1,
                         callbacks=[history]
         )
 
-def save_cnn(hyperparameters, batch_size=32, lr=0.001):
-
-    allele_groups, df = load_binding_data('pan_allele/files/bdata.2009.mhci.public.1.txt')
-
-    create_fasta_file(path, remove_residues = False, consensus_cutoff =0)
-    allele_sequence_data, max_allele_length = load_allele_sequence_data('pan_allele/files/pseudo/pseudo_sequences.fasta')
-    allele_list = sorted(create_allele_list(allele_groups, allele_sequence_data))
-
-    peptide_train, mhc_train, Y_train = get_model_data(allele_list,
-                                                        allele_sequence_data,
-                                                        allele_groups,
-                                                        dense_mhc_model=None,
-                                                        peptide_length = 9,
-                                                        mhc_length=max_allele_length,
-                                                        mhc_dense = None
-                                                        )
-
-
-    optimizer = keras.optimizers.RMSprop(lr=lr, rho=0.9, epsilon=1e-6)
-    graph = convolution_graph_matrix(hyperparameters, maxlen_mhc=max_allele_length, optimizer=optimizer)
-    history = LossHistory()
-    history.metrics(batch_size, lr)
-    graph.fit(
-                        {'peptide':peptide_train, 'mhc':mhc_train, 'output': Y_train},
-                        batch_size=batch_size,
-                        nb_epoch=64,
-                        verbose = 1,
-                        callbacks = [history]
-    )
-
-
 def main():
-    #hyperparameters =  {'cutoff':[ 0], 'dropouts': [ 0.17621593,  0.        ,  0.   ], 'sizes': [ 16, 128,  99, 128, 102], 'mult_size': [32, 15]}
-    #hyperparameters = {'cutoff':[ 0.33711265], 'dropouts': [ 0. ,  0.0254818 ,  0.10669398], 'sizes': [ 53,  82, 103,  74, 106, 59]}
-    hyperparameters = {'cutoff':[ 0.33711265], 'dropouts': [ 0. ,  0.0254818 ,  0.10669398], 'sizes': [ 32,  32, 64,  32, 64, 32]}
-
-    #hyperparameters = {'filter_length': [3, 4], 'nb_filter': [67, 92], 'mult_size': [32, 10], 'layer_size': [ 128, 92, 65]}
-
+    args = parser.parse_args()
+    print args.pred
+    graph = get_graph_from_hyperparameters(args.pred)
+    print graph
     batch_sizes = [32]
-    learning_rates = [ 0.001]
+    learning_rates = [0.001]
 
     for lr in learning_rates:
         for batch_size in batch_sizes:
-            save_ffn(hyperparameters, batch_size, lr)
+            save_model(graph, args.batch_size, args.epochs)
 
 
 if __name__ == "__main__":
--- metrics.py
+++ metrics.py
@@ -9,7 +9,6 @@ import os
 path = os.getcwd()
 sys.path.append(path)
 
-from argparse import ArgumentParser
 
 from pan_allele.helpers.hyperparameters import get_graph_from_hyperparameters
 from pan_allele.helpers.peptide_trim import make_prediction
@@ -21,11 +20,27 @@ import numpy as np
 import collections
 import pandas as pd
 import csv
+import argparse
+
+
 
 
 max_ic50 = 20000
 ic50_cutoff = 500
 log_transformed_ic50_cutoff = 1 - np.log(ic50_cutoff)/np.log(max_ic50)
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--pred",
+    default='ffn_mult',
+    help="neural network type, `ffn_concat`, `ffn_mult` or `conv_mult`")
+
+parser.add_argument(
+    "--epoch",
+    default=25,
+    help="model at which epoch to choose")
 
 
 def scores(Y_true, Y_pred):
@@ -63,71 +78,70 @@ def read_blind_predictions(filename):
 def main():
 
     #prediction input either "conv", "ffn_concat", "ffn_mult"
-    pred = sys.argv[1]
-    graph = get_graph_from_hyperparameters(pred)
+    args = parser.parse_args()
 
-    ##Load graph
-    for num in range(34,75):
-        predictors = ['mhcflurry', 'netmhcpan', 'netmhc', 'smmpmbec_cpp']
-        #allele_list
+    graph = get_graph_from_hyperparameters(args.pred)
 
-        allele_list = ['A0101',	    'A0201',	'A0202',    'A0203',	'A0206',	'A0301',
-                       'A1101',	    'A2301',	'A2402',	'A2501',	'A2601',    'A2602',
-                       'A2603',	    'A2902',	'A3001',	'A3002',	'A3101',	'A3201',
-                       'A3301',	    'A6801',	'A6802',	'A6901',    'A8001',	'B0702',
-                       'B0801',	    'B0802',	'B0803',	'B1501',	'B1503',    'B1509',
-                       'B1517',	    'B1801',	'B2703',    'B2705',    'B3501',	'B3801',
-                       'B3901',	    'B4001',	'B4002',	'B4402',	'B4403',	'B4501',
-                       'B4601',	    'B5101',    'B5301',	'B5401',	'B5701',	'B5801'	]
+    predictors = ['mhcflurry', 'netmhcpan', 'netmhc', 'smmpmbec_cpp']
+    #allele_list
 
+    allele_list = ['A0101',	    'A0201',	'A0202',    'A0203',	'A0206',	'A0301',
+                   'A1101',	    'A2301',	'A2402',	'A2501',	'A2601',    'A2602',
+                   'A2603',	    'A2902',	'A3001',	'A3002',	'A3101',	'A3201',
+                   'A3301',	    'A6801',	'A6802',	'A6901',    'A8001',	'B0702',
+                   'B0801',	    'B0802',	'B0803',	'B1501',	'B1503',    'B1509',
+                   'B1517',	    'B1801',	'B2703',    'B2705',    'B3501',	'B3801',
+                   'B3901',	    'B4001',	'B4002',	'B4402',	'B4403',	'B4501',
+                   'B4601',	    'B5101',    'B5301',	'B5401',	'B5701',	'B5801'	]
 
 
-        #Load graph
-        lr = 0.001
-        batch_size = 32
-        graph.load_weights('weights/' + pred + '/weights' + str(batch_size)+ '_' + str(lr) + '_'  + str(num) )
 
-        #Initializing
-        data_len = sum(len(read_blind_predictions('combined-test-data/'+ allele + '.csv').keys()) for allele in allele_list)
-        Y_true_all = np.zeros(data_len)
-        total_metrics = collections.defaultdict(list)
+    #Load graph
+    lr = 0.001
+    batch_size = 32
+    graph.load_weights('weights/' + pred + '/weights' + str(batch_size)+ '_' + str(lr) + '_'  + str(args.epoch) )
+
+    #Initializing
+    data_len = sum(len(read_blind_predictions('combined-test-data/'+ allele + '.csv').keys()) for allele in allele_list)
+    Y_true_all = np.zeros(data_len)
+    total_metrics = collections.defaultdict(list)
+    for val in predictors:
+            total_metrics[val] =  np.zeros(data_len)
+    pos  = 0
+    calculated_metrics =collections.defaultdict(tuple)
+    for val in predictors:
+        calculated_metrics[val] = (0,0,0,0,0,0)
+
+
+
+    #calculating metrics per allele
+    for allele in allele_list:
+        filename = 'combined-test-data/'+ allele + '.csv'
+        predictions = read_blind_predictions(filename)
+        peptides = predictions.keys()
+        for peptide in peptides:
+            predictions[peptide]['mhcflurry'] = 20000**(1-make_prediction(peptide, allele_sequence_data[allele], graph))
+        df_pred = pd.DataFrame(predictions)
+
+
+        Y_true_allele = np.array(df_pred.loc['meas'])
+        Y_true_all[pos:pos+len(peptides)] =  Y_true_allele
+
+        #print "\n=====", allele, sum(Y_true_allele <= 500), len(Y_true_allele), "===="
         for val in predictors:
-                total_metrics[val] =  np.zeros(data_len)
-        pos  = 0
-        calculated_metrics =collections.defaultdict(tuple)
-        for val in predictors:
-            calculated_metrics[val] = (0,0,0,0,0,0)
+            Y_pred_allele = np.array(df_pred.loc[val])
+            calculated_metrics[val]  = map(sum, zip(scores(Y_true_allele, Y_pred_allele), calculated_metrics[val]))
+            print val, scores(Y_true_allele, Y_pred_allele)
+            total_metrics[val][pos:pos+len(peptides)] = (Y_pred_allele)
 
+        pos +=len(peptides)
+    print calculated_metrics
+    print "\n",num
 
-
-        #calculating metrics per allele
-        for allele in allele_list:
-            filename = 'combined-test-data/'+ allele + '.csv'
-            predictions = read_blind_predictions(filename)
-            peptides = predictions.keys()
-            for peptide in peptides:
-                predictions[peptide]['mhcflurry'] = 20000**(1-make_prediction(peptide, allele_sequence_data[allele], graph))
-            df_pred = pd.DataFrame(predictions)
-
-
-            Y_true_allele = np.array(df_pred.loc['meas'])
-            Y_true_all[pos:pos+len(peptides)] =  Y_true_allele
-
-            #print "\n=====", allele, sum(Y_true_allele <= 500), len(Y_true_allele), "===="
-            for val in predictors:
-                Y_pred_allele = np.array(df_pred.loc[val])
-                calculated_metrics[val]  = map(sum, zip(scores(Y_true_allele, Y_pred_allele), calculated_metrics[val]))
-                print val, scores(Y_true_allele, Y_pred_allele)
-                total_metrics[val][pos:pos+len(peptides)] = (Y_pred_allele)
-
-            pos +=len(peptides)
-        print calculated_metrics
-        print "\n",num
-
-        for val in predictors:
-            print "\n",val
-            scores_val = scores(Y_true_all, total_metrics[val])
-            print scores_val
+    for val in predictors:
+        print "\n",val
+        scores_val = scores(Y_true_all, total_metrics[val])
+        print scores_val
 
 if __name__ == "__main__":
     main()
