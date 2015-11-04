0e3e5dc8e3b190cb566fda94e21bf4447997103f
NanditaDamaraju <NanditaDamaraju@mssm.edu>
2015-11-04 16:48:58 -0500

    conv_mult doesn’t show all alleles

--- pan_allele/helpers/generate_pseudo_sequences.py
+++ pan_allele/helpers/generate_pseudo_sequences.py
@@ -44,7 +44,7 @@ def create_fasta_file(remove_residues = False, consensus_cutoff = 0):
 
     else:
 
-        with open("pan_allele/files/trimmed-human-class1-IEDB.fasta",'rU') as f:
+        with open("pan_allele/files/trimmed-human-class1.fasta",'rU') as f:
             with open("pan_allele/files/pseudo/pseudo_sequences.fasta", "wb") as f1:
                 for line in f:
                     f1.write(line)
